f5dd56d437e236320f29f53e467c2b0e60eb8d82
Les Aker <me@lesaker.org>
2019-07-11 23:23:14 +0000

    update module

--- main.tf
+++ main.tf
@@ -24,7 +24,7 @@ data "aws_iam_policy_document" "file_bucket_read_access" {
 
 module "certificate" {
   source    = "armorfret/acm-certificate/aws"
-  version   = "0.1.1"
+  version   = "0.1.2"
   hostnames = concat(list(var.primary_hostname), var.redirect_hostnames)
 }
 
